f6f7011cbf618d769fdc06a7cd1d39da5af9bb76
Shivanshu Patel <shivanshu@giffgaff.co.uk>
2019-09-05 12:18:48 +0100

    Don't send notification to all followers for private tags

--- src/Listeners/SendNotificationWhenDiscussionIsStarted.php
+++ src/Listeners/SendNotificationWhenDiscussionIsStarted.php
@@ -48,19 +48,32 @@ class SendNotificationWhenDiscussionIsStarted
             return;
         }
 
-        $notify = User::where('users.id', '!=', $discussion->user_id)
-            ->join('tag_user', 'tag_user.user_id', '=', 'users.id')
-            ->whereIn('tag_user.tag_id', $tagIds->all())
-            ->whereIn('tag_user.subscription', ['follow', 'lurk'])
-            ->get()
-            ->reject(function ($user) use ($tags) {
-                return $tags->map->state->map->subscription->contains('ignore')
-                    || $tags->whereIn('id', Tag::getIdsWhereCannot($user, 'viewDiscussions'))->isNotEmpty();
-            });
+        if ($discussion->is_private) {
+            foreach ($discussion->recipientUsers()->get() as $recipientUser) {
+                if ($recipientUser->id === $event->actor->id) {
+                    continue;
+                }
+
+                $notify[] = $recipientUser;
+            }
+        }
+
+        if (!$discussion->is_private) {
+            $notify = User::where('users.id', '!=', $discussion->user_id)
+                ->join('tag_user', 'tag_user.user_id', '=', 'users.id')
+                ->whereIn('tag_user.tag_id', $tagIds->all())
+                ->whereIn('tag_user.subscription', ['follow', 'lurk'])
+                ->get()
+                ->reject(function ($user) use ($tags) {
+                    return $tags->map->state->map->subscription->contains('ignore')
+                        || $tags->whereIn('id', Tag::getIdsWhereCannot($user, 'viewDiscussions'))->isNotEmpty();
+                })
+                ->all();
+        }
 
         $this->notifications->sync(
             new NewDiscussionBlueprint($discussion),
-            $notify->all()
+            $notify
         );
     }
 }
