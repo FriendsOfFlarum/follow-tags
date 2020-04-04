8b92630bcdf799990c9eae8e1ce136adb47bf850
datitisev <me@datitisev.me>
2020-04-04 14:08:25 +0000

    Apply fixes from StyleCI

--- src/Jobs/SendNotificationWhenDiscussionIsStarted.php
+++ src/Jobs/SendNotificationWhenDiscussionIsStarted.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Collection;
 
 class SendNotificationWhenDiscussionIsStarted implements ShouldQueue
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     /**
      * @var Discussion
