af3d2718ab303d1889ac1c5454c79ca95dddf792
Daniël Klabbers <daniel@klabbers.email>
2020-06-25 07:59:58 +0200

    support fallback

--- src/Listeners/AddTagSubscriptionAttribute.php
+++ src/Listeners/AddTagSubscriptionAttribute.php
@@ -13,13 +13,16 @@ namespace FoF\FollowTags\Listeners;
 
 use Flarum\Api\Event\Serializing;
 use Flarum\Tags\Api\Serializer\TagSerializer;
+use Illuminate\Support\Arr;
 
 class AddTagSubscriptionAttribute
 {
     public function handle(Serializing $event)
     {
         if ($event->isSerializer(TagSerializer::class)) {
-            $event->attributes['subscription'] = $event->model->state['subscription'];
+            $state = $event->model->state ?? $event->model->stateFor($event->actor) ?? [];
+
+            $event->attributes['subscription'] = Arr::get($state, 'subscription');
         }
     }
 }
