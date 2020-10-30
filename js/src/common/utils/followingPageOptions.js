72d06ba2271ae5798ff31cd92e800ffadf88fd21
David Sevilla Martin <me@datitisev.me>
2020-10-29 20:03:36 -0400

    Remove option to show both followed tags & discussions

--- js/src/common/utils/followingPageOptions.js
+++ js/src/common/utils/followingPageOptions.js
@@ -2,7 +2,7 @@ let opts;
 
 export default (section) =>
     opts ||
-    (opts = ['none', 'tags', 'all'].reduce((o, key) => {
+    (opts = ['none', 'tags'].reduce((o, key) => {
         o[key] = app.translator.trans(`fof-follow-tags.${section}.following_${key}_label`);
 
         return o;
