55fd5dc69a92e6d004fe520fb0284d1127b8f5a2
karaok <karaok491@gmail.com>
2020-11-11 20:42:17 +0000

    Fix blade for new post notification

--- resources/views/emails/newPost.blade.php
+++ resources/views/emails/newPost.blade.php
@@ -3,7 +3,7 @@ Hey {!! $user->display_name !!}!
 {!! $blueprint->post->user->display_name !!} made a post in a discussion on a tag you're following: {!! $blueprint->post->discussion->title !!}
 
 To view the new activity, check out the following link:
-{!! $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]) !!}
+{!! $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]) !!}
 
 ---
 
