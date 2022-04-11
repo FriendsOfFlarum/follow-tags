1a864f9fe5cb8daec0006a5f8dbdf5899aeac123
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2022-04-11 13:55:55 -0400

    fix: avoid errors if flarum subscriptions isn't enabled
    This isn't extremely robust, as the initializer key name could change at any point and isn't really considered public API. But without this, tags don't currently load.

--- js/src/forum/utils/isFollowingPage.js
+++ js/src/forum/utils/isFollowingPage.js
@@ -1,3 +1,7 @@
 import app from 'flarum/forum/app';
 
-export default () => m.route.get().includes(app.route('following'));
+export default () => {
+    if (!app.initializers.has('subscriptions')) return;
+
+    return m.route.get().includes(app.route('following'));
+}
