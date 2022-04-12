d115172af7e850af1577cd5931fd6ab110ffcf02
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2022-04-11 22:10:36 -0400

    Update isFollowingPage.js

--- js/src/forum/utils/isFollowingPage.js
+++ js/src/forum/utils/isFollowingPage.js
@@ -1,3 +1,3 @@
 import app from 'flarum/forum/app';
 
-export default () => 'flarum-subscriptions' in flarum.extensions && m.route.get().includes(app.route('following'))`
+export default () => 'flarum-subscriptions' in flarum.extensions && m.route.get().includes(app.route('following'))
