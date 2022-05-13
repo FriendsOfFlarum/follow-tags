02cd568fd962db67f101d6583e34924b17a492f1
Ian Morland <ian@morland.me>
2022-05-13 22:06:20 +0100

    Add excerpt

--- js/src/forum/components/NewDiscussionTagNotification.js
+++ js/src/forum/components/NewDiscussionTagNotification.js
@@ -19,4 +19,8 @@ export default class NewDiscussionTagNotification extends Notification {
       title: this.attrs.notification.subject().title(),
     });
   }
+
+  excerpt() {
+    return null;
+  }
 }
