e5ddbd4438726db95e2a94a93d351cec4af05ea2
Rafael Horvat <rafael.horvat@glowingblue.com>
2022-05-20 18:55:05 +0200

    Add timestamps to `tag_user`

---
+++ migrations/2022_05_20_000000_add_timestamps_to_tag_user_table.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of fof/follow-tags.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Flarum\Database\Migration;
+
+return Migration::addColumns('tag_user', [
+    'created_at' => [
+        'timestamp',
+        'useCurrent' => true,
+    ],
+    'updated_at' => [
+        'timestamp',
+        'useCurrent' => true,
+        'useCurrentOnUpdate' => true,
+    ],
+]);
