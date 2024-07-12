f11d7e1323f083fbbefad5c986bfae54e1b6eb97
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-07-12 16:59:19 -0400

    Retrieve the *correct* tag state for the right user (#73)
    * Retrieve the *correct* tag state for the right user
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/AddTagSubscriptionAttribute.php
+++ src/AddTagSubscriptionAttribute.php
@@ -14,16 +14,34 @@ namespace FoF\FollowTags;
 use Flarum\Tags\Api\Serializer\TagSerializer;
 use Flarum\Tags\Tag;
 use Flarum\Tags\TagState;
+use Flarum\User\User;
 
 class AddTagSubscriptionAttribute
 {
     public function __invoke(TagSerializer $serializer, Tag $tag, array $attributes): array
     {
-        /** @var TagState $state */
-        $state = $tag->state;
+        $state = $this->getStateFor($tag, $serializer->getActor());
 
         $attributes['subscription'] = $state->subscription ?? null;
 
         return $attributes;
+    }
+
+    /**
+     * Get the *correct* state for the tag based on the actor.
+     * If a state hasn't been already loaded OR the loaded state is not for the actor, load the correct state.
+     *
+     * @param Tag  $tag
+     * @param User $actor
+     *
+     * @return TagState
+     */
+    public function getStateFor(Tag $tag, User $actor): TagState
+    {
+        if (!$tag->relationLoaded('state') || $tag->state->user_id !== $actor->id) {
+            $tag->setRelation('state', $tag->stateFor($actor));
+        }
+
+        return $tag->state;
     }
 }
