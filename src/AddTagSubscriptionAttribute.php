186b695d55fd8928faa38f20575a64dbccf31ba9
David Sevilla Martin <6401250+dsevillamartin@users.noreply.github.com>
2024-07-13 11:03:30 -0400

    Ignore phpstan is_null error

--- src/AddTagSubscriptionAttribute.php
+++ src/AddTagSubscriptionAttribute.php
@@ -41,6 +41,7 @@ class AddTagSubscriptionAttribute
         // If $tag->state is loaded *and* null, this might be because the actor doesn't have tag-specific state
         // OR because the wrong actor has been used for loading it. `$tag->stateFor()` will return the correct state.
         // If it doesn't exist, it returns a dummy state with the correct actor & tag IDs.
+        // @phpstan-ignore-next-line ($tag->state may indeed be null)
         if (!$tag->relationLoaded('state') || is_null($tag->state) || $tag->state->user_id !== $actor->id) {
             $tag->setRelation('state', $tag->stateFor($actor));
         }
