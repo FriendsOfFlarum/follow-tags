d1bb5ebadcaaaee0bc5bf498fea7b261c0ff6698
Sami Mazouz <sychocouldy@gmail.com>
2024-11-15 14:15:51 +0100

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/forum/addSubscriptionControls.js
+++ js/src/forum/addSubscriptionControls.js
@@ -1,3 +1,4 @@
+import IndexSidebar from 'flarum/forum/components/IndexSidebar';
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import Model from 'flarum/common/Model';
@@ -8,10 +9,10 @@ import SubscriptionStateButton from './components/SubscriptionStateButton';
 export default () => {
   app.store.models.tags.prototype.subscription = Model.attribute('subscription');
 
-  extend(IndexPage.prototype, 'sidebarItems', function (items) {
-    if (!this.currentTag() || !app.session.user) return;
+  extend(IndexSidebar.prototype, 'items', function (items) {
+    if (!app.currentTag() || !app.session.user) return;
 
-    const tag = this.currentTag();
+    const tag = app.currentTag();
 
     if (items.has('newDiscussion')) items.setPriority('newDiscussion', 10);
 
