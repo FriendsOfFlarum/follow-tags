43a8800f76a34c1308965ceec0ffb1121b8238b8
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-01-24 12:00:44 +0100

    test: write tests for amount of notifications received

--- tests/integration/TagsDefinitionTrait.php
+++ tests/integration/TagsDefinitionTrait.php
@@ -20,6 +20,9 @@ trait TagsDefinitionTrait
             ['id' => 2, 'name' => 'Testing', 'slug' => 'testing', 'position' => 1, 'parent_id' => null],
             ['id' => 3, 'name' => 'Playground', 'slug' => 'playground', 'position' => 1, 'parent_id' => null],
             ['id' => 4, 'name' => 'Archive', 'slug' => 'archive', 'position' => 2, 'parent_id' => null, 'is_restricted' => true],
+            ['id' => 5, 'name' => 'General Child', 'slug' => 'general-child', 'position' => 0, 'parent_id' => 1],
+            ['id' => 6, 'name' => 'Testing Child', 'slug' => 'testing-child', 'position' => 0, 'parent_id' => 2],
+
         ];
     }
 }
