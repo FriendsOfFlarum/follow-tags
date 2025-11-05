3059f79e08ce89c96fddd1f3c7a2753df9549d04
IanM <ian@morland.me>
2025-11-05 20:31:21 +0000

    chore(2.0): use new import from extension format
    Importing modules from an extension can now be done though `ext:vendor/extension/module-path` format.

--- js/src/forum/components/SubscriptionModal.tsx
+++ js/src/forum/components/SubscriptionModal.tsx
@@ -5,7 +5,7 @@ import ItemList from 'flarum/common/utils/ItemList';
 import { utils } from '../utils';
 import SubscriptionOptionItem from './SubscriptionOptionItem';
 import type Mithril from 'mithril';
-import Tag from 'flarum/tags/models/Tag';
+import Tag from 'ext:flarum/tags/models/Tag';
 import Stream from 'flarum/common/utils/Stream';
 import Tooltip from 'flarum/common/components/Tooltip';
 
