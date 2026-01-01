2a357b75bd01e04abbc26a1be1f4edc248ed3e40
IanM <16573496+imorland@users.noreply.github.com>
2026-01-01 17:09:33 +0000

    fix: remove email workaround needed for earlier betas (#91)

--- resources/views/email/html/newPost.blade.php
+++ resources/views/email/html/newPost.blade.php
@@ -1,7 +1,13 @@
-<x-mail::html.notification :preview="$blueprint->post->formatContent()">
-    {!! $formatter->convert($translator->trans('fof-follow-tags.email.newPostInTag.html.body', [
-        '{actor_display_name}' => $blueprint->post->user->display_name,
-        '{discussion_title}' => $blueprint->post->discussion->title,
-        '{post_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]),
-    ])) !!}
+<x-mail::html.notification>
+    <x-slot:body>
+        {!! $formatter->convert($translator->trans('fof-follow-tags.email.newPostInTag.html.body', [
+            '{actor_display_name}' => $blueprint->post->user->display_name,
+            '{discussion_title}' => $blueprint->post->discussion->title,
+            '{post_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]),
+        ])) !!}
+    </x-slot:body>
+
+    <x-slot:preview>
+        {!! $blueprint->post->formatContent() !!}
+    </x-slot:preview>
 </x-mail::html.notification>
--- resources/views/email/html/newTag.blade.php
+++ resources/views/email/html/newTag.blade.php
@@ -1,8 +1,14 @@
-<x-mail::html.notification :preview="$blueprint->post->formatContent()">
-    {!! $formatter->convert($translator->trans('fof-follow-tags.email.newDiscussionTag.html.body', [
-        '{actor_display_name}' => $blueprint->actor->display_name,
-        '{author_display_name}' => $blueprint->discussion->user->display_name,
-        '{discussion_title}' => $blueprint->discussion->title,
-        '{discussion_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]),
-    ])) !!}
+<x-mail::html.notification>
+    <x-slot:body>
+        {!! $formatter->convert($translator->trans('fof-follow-tags.email.newDiscussionTag.html.body', [
+            '{actor_display_name}' => $blueprint->actor->display_name,
+            '{author_display_name}' => $blueprint->discussion->user->display_name,
+            '{discussion_title}' => $blueprint->discussion->title,
+            '{discussion_url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->discussion->id]),
+        ])) !!}
+    </x-slot:body>
+
+    <x-slot:preview>
+        {!! $blueprint->post->formatContent() !!}
+    </x-slot:preview>
 </x-mail::html.notification>
